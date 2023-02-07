1b673ea3c8cd835fb62a148e7bbf47db6a7dd755
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-07 16:29:04 -0500

    set up signServices and NewSign page (create)

---
+++ src/pages/NewSign/NewSign.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import styles from './NewSign.module.css'
+
+const NewSign = (props) => {
+  const [form, setForm] = useState({
+    noun: '',
+    adjective: '',
+  })
+
+  const handleChange = ({ target }) => {
+    setForm({ ...form, [target.name]: target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.handleAddSign(form)
+  }
+
+  return ( 
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title-input">Title</label>
+        <input
+          required
+          type="text"
+          name="noun"
+          id="noun-input"
+          value={form.noun}
+          placeholder="Noun"
+          onChange={handleChange}
+        />
+        <label htmlFor="text-input">Text</label>
+				<textarea
+          required
+          type="text"
+          name="adjective"
+          id="Adjective-input"
+          value={form.adjective}
+          placeholder="Adjective"
+          onChange={handleChange}
+        />
+        <button type="submit">SUBMIT</button>
+      </form>
+    </main>
+
+  )
+}
+
+export default NewSign;
