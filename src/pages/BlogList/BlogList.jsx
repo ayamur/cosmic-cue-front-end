d18310ed18e03dc287918d536a8ad8ca9f090599
AyaMu <ayalamuruch@gmail.com>
2023-02-06 19:20:48 -0500

    stub up BlogList.jsx component

---
+++ src/pages/BlogList/BlogList.jsx
@@ -0,0 +1,12 @@
+// import styles from './BlogList.modules.css'
+const BlogList = (props) => {
+  return (
+    <>
+      <main className={styles.container}>
+        <h4>Blog List</h4>
+      </main>
+    </>
+  )
+}
+
+export default BlogList
