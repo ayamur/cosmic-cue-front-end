c505acc535e4c607cc843848ebabd777034a9deb
AyaMu <ayalamuruch@gmail.com>
2023-02-08 16:31:54 -0500

    update NewBlog.jsx

---
+++ src/pages/NewBlog/NewBlog.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import styles from './NewBlog.module.css'
+
+const NewBlog = (props) => {
+  const [form, setForm] = useState({
+    title: '',
+    text: '',
+  })
+
+  const handleChange = ({ target }) => {
+    setForm({ ...form, [target.name]: target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+		// Update this line shortly...
+  }
+
+  return (
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title-input">Manifestation</label>
+        <input
+          required
+          type="text"
+          name="title"
+          id="title-input"
+          value={form.title}
+          placeholder="Title"
+          onChange={handleChange}
+        />
+        <label htmlFor="text-input">Mantra</label>
+				<textarea
+          required
+          type="text"
+          name="text"
+          id="text-input"
+          value={form.text}
+          placeholder="Text"
+          onChange={handleChange}
+        />
+      </form>
+    </main>
+  )
+}
+
+export default NewBlog
