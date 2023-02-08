0cdbba768f36916db37c01bcf646aef873b4822e
AyaMu <ayalamuruch@gmail.com>
2023-02-08 14:53:21 -0500

    update BlogList with BlogCard import

--- src/pages/BlogList/BlogList.jsx
+++ src/pages/BlogList/BlogList.jsx
@@ -1,12 +1,11 @@
 import styles from './BlogList.module.css'
+import BlogCard from "../../components/BlogCard/BlogCard"
 
 const BlogList = (props) => {
   return (
     <main className={styles.container}>
       {props.blogs.map((blog) => (
-        <div key={blog._id}>
-          {blog.title}
-        </div>
+        <BlogCard key={blog._id} blog={blog} />
       ))}
     </main>
   )
