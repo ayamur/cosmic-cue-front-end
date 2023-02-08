ee0e7d8f4fe11b300d588217089331d978128f62
AyaMu <ayalamuruch@gmail.com>
2023-02-08 17:25:59 -0500

    update TypeError fix in BlogDetails for 'title'

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -21,14 +21,13 @@ const BlogDetails = (props) => {
     fetchBlog()
   }, [id])
 
-  // if (!blog) return <Loading />
+  if (!blog) return <Loading />
 
   return (
     <main className={styles.container}>
       <article>
         <header>
-          <h3>{blog.category.toUpperCase()}</h3>
-          <h1>{blog.title}</h1>
+          <h1>{blog?.title}</h1>
           <span>
             <AuthorInfo content={blog} />
           </span>
